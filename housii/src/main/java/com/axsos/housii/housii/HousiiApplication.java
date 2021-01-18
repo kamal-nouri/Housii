6c7d0d8b9dee990982dd785f7aba2457da1429a9
kamal <kamal@email.com>
2021-01-18 17:55:36 +0200

    adding the project

---
+++ housii/src/main/java/com/axsos/housii/housii/HousiiApplication.java
@@ -0,0 +1,13 @@
+package com.axsos.housii.housii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousiiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousiiApplication.class, args);
+    }
+
+}
---
+++ housii/src/test/java/com/axsos/housii/housii/HousiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axsos.housii.housii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousiiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
