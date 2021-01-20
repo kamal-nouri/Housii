9ddb3c003c69d96c48594f2fbae97f1d462f050e
kamal <kamal@email.com>
2021-01-20 11:19:49 +0200

    updating the apicontroller

--- housii/src/main/java/com/axsos/housii/housii/services/HousiiService.java
+++ housii/src/main/java/com/axsos/housii/housii/services/HousiiService.java
@@ -1,10 +1,14 @@
 package com.axsos.housii.housii.services;
 
 
+import com.axsos.housii.housii.models.Category;
 import com.axsos.housii.housii.models.User;
 import com.axsos.housii.housii.repositories.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class HousiiService {
@@ -42,6 +46,46 @@ public class HousiiService {
     // 3
     public User findByUsername(String username) {
         return userRepository.findByName(username);
+    }
+
+    //User services
+    public List<User> allUsers() {
+        return userRepository.findAll();
+    }
+
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+
+    public User findUser(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            return optionalUser.get();
+        } else {
+            return null;
+        }
+    }
+
+    //    Category services
+    public List<Category> allCategories() {
+        return categoryRepository.findAll();
+    }
+
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+
+    public Category findCategory(Long id) {
+        Optional<Category> optionalCategory = categoryRepository.findById(id);
+        if (optionalCategory.isPresent()) {
+            return optionalCategory.get();
+        } else {
+            return null;
+        }
     }
 }
 
